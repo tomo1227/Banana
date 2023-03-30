a27b6cea6c5c7dc0054fa6cbea71842d8076933a
Tomoki Ota <tomo.soccer1227@gmail.com>
2023-03-31 02:53:21 +0900

    Initial Commit

---
+++ Banana/BananaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BananaApp.swift
+//  Banana
+//
+//  Created by Tomoki Ota on 2023/03/31.
+//
+
+import SwiftUI
+
+@main
+struct BananaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
